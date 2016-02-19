66ad8f62b22a9e8915da51d2ed178083e75fa423
Ojas Shirekar <ojas.shirekar@gmail.com>
2016-02-19 11:26:13 +0530

    follow: using dictionary now
    now using a dictionary to store the follows.

--- first_follow.py
+++ first_follow.py
@@ -2,6 +2,7 @@
 # Author Ojas Shirekar
 # Under the GPL license
 from collections import OrderedDict
+
 # Opening the rules file, doing it the hard coded way for development
 
 
@@ -21,12 +22,13 @@ def non_term_appender(firsts, rules):
         if rule[0][0] not in firsts:
             firsts.append(rule[0][0])
             firsts_dict[rule[0][0]] = []
+            follow_dict[rule[0][0]] = []
 
 
 with open("rules_test.txt", "r") as fp:
     for line in fp:
-        rules.append(line.strip().split('\n'))  # Splitting on newline and
-    # and turning it into an array
+        # Splitting on newline and turning it into an array
+        rules.append(line.strip().split('\n'))
 
 number_of_rules = len(rules)
 # Printing the array
@@ -38,7 +40,7 @@ for first in firsts:
 
 for rule in rules:
     if rule[0][3].islower():
-        firsts_dict[rule[0][0]].append(rule[0][3])
+        firsts_dict[rule[0][0]].extend(rule[0][3])
 # TODO try and re implement one pass, as an else condition in the above loop.
 for rule in rules:
     if rule[0][3].isupper():
@@ -48,3 +50,16 @@ with open("firsts.txt", "w+") as wp:
     for k in firsts_dict:
         wp.write("first(%s): \t " % k)
         wp.write("%s\n" % firsts_dict[k])
+# Following code is used to find the follows
+rules_keys = rules_dict.keys()
+key_count = len(rules_keys)
+# Use number_of_rules here!
+for k in rules_dict:
+    tmp_rule_str = rules_dict[k]
+    if k == rules_keys[0]:
+        follow_dict[k].append('$')
+    for i in xrange(key_count):
+        if rules_keys[i] in tmp_rule_str:
+            follow_dict[rules_keys[i]].extend(
+                firsts_dict[rules_keys[(i + 1) % key_count]])
+print follow_dict
