7ef02a0eb261d0b011e441cb404c98c457b71d45
Ojas Shirekar <ojas.shirekar@gmail.com>
2016-02-19 10:31:21 +0530

    Add comments for script description. lint the code.

--- first_follow.py
+++ first_follow.py
@@ -1,3 +1,4 @@
+"""Finds the first and follow for a given grammar"""
 # Author Ojas Shirekar
 # Under the GPL license
 from collections import OrderedDict
@@ -7,15 +8,17 @@ from collections import OrderedDict
 rules = []  # Raw grammar rules
 firsts = []
 
-# Ordered dictionary to maintain the order of saving in the dictionary, useful when doing the actual
-# first finding in one pass.
+# Ordered dictionary to maintain the order of saving in the dictionary
+# useful when doing the actual first finding in one pass.
 rules_dict = OrderedDict()  # Dictionary to store all the rules in the grammar
 firsts_dict = OrderedDict()  # Dictionary to store all the firsts
+follow_dict = OrderedDict()  # Dictionary that stores all follows
 
 
 def non_term_appender(firsts, rules):
+    """Adds the non terminals on the left to the firsts_dict"""
     for rule in rules:
-        if (rule[0][0] not in firsts):
+        if rule[0][0] not in firsts:
             firsts.append(rule[0][0])
             firsts_dict[rule[0][0]] = []
 
@@ -23,7 +26,7 @@ def non_term_appender(firsts, rules):
 with open("rules_test.txt", "r") as fp:
     for line in fp:
         rules.append(line.strip().split('\n'))  # Splitting on newline and
-        # and turning it into an array
+    # and turning it into an array
 
 number_of_rules = len(rules)
 # Printing the array
@@ -34,7 +37,7 @@ for first in firsts:
     rule_count += 1
 
 for rule in rules:
-    if (rule[0][3].islower()):
+    if rule[0][3].islower():
         firsts_dict[rule[0][0]].append(rule[0][3])
 # TODO try and re implement one pass, as an else condition in the above loop.
 for rule in rules:
@@ -45,5 +48,3 @@ with open("firsts.txt", "w+") as wp:
     for k in firsts_dict:
         wp.write("first(%s): \t " % k)
         wp.write("%s\n" % firsts_dict[k])
-
-print(firsts_dict)
