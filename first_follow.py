ae4dacc3923d2287fdd8e3e40abb9a0bf032a2c9
Ojas Shirekar <ojas.shirekar@gmail.com>
2016-02-20 15:10:49 +0530

    follow: add comments documenting the code

--- first_follow.py
+++ first_follow.py
@@ -30,6 +30,7 @@ with open("rules_test.txt", "r") as fp:
         # Splitting on newline and turning it into an array
         rules.append(line.strip().split('\n'))
 
+# ++++++    Following code is used to find firsts   +++++++++
 number_of_rules = len(rules)
 rule_count = first_count = 0
 non_term_appender(firsts, rules)
@@ -49,7 +50,9 @@ with open("firsts.txt", "w+") as wp:
     for k in firsts_dict:
         wp.write("first(%s): \t " % k)
         wp.write("%s\n" % firsts_dict[k])
-# Following code is used to find the follows
+
+# ++++++    Following code is used to find the follows  ++++++
+
 rules_keys = rules_dict.keys()
 key_count = len(rules_keys)
 
@@ -61,10 +64,18 @@ for k in rules_dict:
         if rules_keys[i] in tmp_rule_str:
             # Follow finding for last non-terminal in the
             tmp_rule_list = list(tmp_rule_str)
+            # stores the index non-terminal we are finding the follow for in a
+            # variable
             current_non_term_index = tmp_rule_list.index(rules_keys[i])
+
             if current_non_term_index == (len(tmp_rule_list) - 1):
+                # if the index of the current non-term is at last one, means that
+                # the follow for the current non-term will be the follow of the
+                # left-hand side
                 follow_dict[rules_keys[i]].extend(follow_dict[rules_keys[0]])
             else:
+                # If not then the first of the next non-term from the current
+                # non-term becomes the follow of the current
                 follow_dict[rules_keys[i]].extend(
                     firsts_dict[rules_keys[(i + 1) % key_count]])
 
@@ -72,4 +83,5 @@ with open("follows.txt", "w+") as wp:
     for k in follow_dict:
         wp.write("follow(%s): \t" % k)
         wp.write("%s\n" % follow_dict[k])
-print follow_dict
+print "Firsts Dict:" + " " + follow_dict
+print "Follow Dict:" + " " + firsts_dict
